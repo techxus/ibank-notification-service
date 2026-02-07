6fd6d2b7c189060ad17f2b3165ee4658be195eea
softnet <daniel@softnet.cloud>
2026-02-07 08:08:31 -0500

    Fixes

--- src/main/java/com/ibank/notification/controller/NotificationController.java
+++ src/main/java/com/ibank/notification/controller/NotificationController.java
@@ -26,7 +26,7 @@ public class NotificationController {
         log.info("Processing GET:/api/v1/notification/server...");
 
         return Map.of(
-                "message", "Brick AWS Setup Milestone Reached !!!",
+                "message", "Brick HTTPS Route53 Setup Milestone Reached !!!",
                 "timestamp", Instant.now().toString(),
                 "counter", counterValue
         );
